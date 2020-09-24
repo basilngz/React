175a586968ee8d5284156d197b6e42f933c4d723
basil ng zerone <basil.g@zerone-consulting.com>
2020-09-25 01:11:00 +0530

    React components part 2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Media } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class Menu extends Component {
 
@@ -7,60 +7,44 @@ class Menu extends Component {
          super(props);
 
          this.state = {
-             dishes: [
-                 {
-                     id: 0,
-                     name: 'Soup',
-                     image: 'assets/images/soup.png',
-                     category: 'starter',
-                     label: 'Hot',
-                     price: '60.0',
-                     description: 'A unique combination of indian'
-                 },
-                 {
-                     id: 1,
-                     name: "Biriyani",
-                     image: 'assets/images/biriyani.png',
-                     category: 'mains',
-                     label: 'Hot',
-                     price: '120.0',
-                     description: 'A good food'
-                 },
-                 {
-                     id: 2,
-                     name: 'Salad',
-                     image: 'assets/images/salad.png',
-                     category: 'side-dish',
-                     label: '',
-                     price: '50.0',
-                     description: 'A good combination with biriyani'
-                 },
-                 {
-                     id: 3,
-                     name: 'Cheese Cake',
-                     image: 'assets/images/cake.png',
-                     category: 'dessert',
-                     label: 'cold',
-                     price: '35.50',
-                     description: "A delectable, semi-sweet new cheese cake"
-                 }
-             ]
+            selectedDish: null
+         }
+     }
+
+     onDishSelect(dish) {
+         this.setState({ selectedDish: dish})
+     }
+
+     renderDish(dish) {
+         if (dish != null) {
+            return(
+                <Card>
+                    <CardImg width="100%" object src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+         }
+         else {
+             return(
+                 <div>
+                 </div>
+             );
          }
      }
      render() {
          
-        const menu = this.state.dishes.map((dish) => {
+        const menu = this.props.dishes.map((dish) => {
             return (
-                <div key={dish.id} className="col-12 mt-5">
-                    <Media tag="li">
-                        <Media left middle>
-                            <Media object src={dish.image} alt={dish.name} />
-                        </Media>
-                        <Media body className="ml-5">
-                            <Media heading>{dish.name}</Media>
-                            <p>{dish.description}</p>
-                        </Media>
-                    </Media>
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <Card onClick={() => this.onDishSelect(dish)}>
+                        <CardImg width="100%" object src={dish.image} alt={dish.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
                 </div>
             );
         });
@@ -68,9 +52,10 @@ class Menu extends Component {
          return (
             <div className="container">
                 <div className="row">
-                    <Media list>
-                        {menu}
-                    </Media>
+                    {menu}
+                </div>
+                <div className="row">
+                    {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
          );
---
+++ src/shared/dishes.js
@@ -0,0 +1,188 @@
+export const DISHES = 
+[
+    {
+        id: 0,
+        name: 'Soup',
+        image: 'assets/images/soup.png',
+        category: 'starter',
+        label: 'Hot',
+        price: '60.0',
+        description: 'A unique combination of indian',
+        comments: [
+            {
+                id: 0,
+                reating: 5,
+                comment: "Imagine all the eatables, living in con",
+                author: "John Lemon",
+                date: "2020-09-25T17:57:28.556094Z"
+            },
+            {
+                id: 1,
+                reating: 4,
+                comment: "Sends anyone to heaven",
+                author: "Paul McVites",
+                date: "2020-09-25T17:57:28.556094Z"
+            },
+            {
+                id: 2,
+                reating: 3,
+                comment: "Eat it, just eat it!",
+                author: "Michael Jaikishan",
+                date: "2020-09-25T17:57:28.556094Z"
+            },
+            {
+                id: 3,
+                reating: 4,
+                comment: "Ultimate, Reaching got the stars!",
+                author: "Ringo Starry",
+                date: "2020-09-25T17:57:28.556094Z"
+            },
+            {
+                id: 4,
+                reating: 1,
+                comment: "It's your birthday, we're gonna party",
+                author: "25 Cent",
+                date: "2020-09-25T17:57:28.556094Z"
+            }
+        ]
+    },
+    {
+        id: 1,
+        name: "Biriyani",
+        image: 'assets/images/biriyani.png',
+        category: 'mains',
+        label: 'Hot',
+        price: '120.0',
+        description: 'A good food',
+        comments: [
+            {
+                id: 0,
+                reating: 5,
+                comment: "Imagine all the eatables, living in con",
+                author: "John Lemon",
+                date: "2020-09-25T17:57:28.556094Z"
+            },
+            {
+                id: 1,
+                reating: 4,
+                comment: "Sends anyone to heaven",
+                author: "Paul McVites",
+                date: "2020-09-25T17:57:28.556094Z"
+            },
+            {
+                id: 2,
+                reating: 3,
+                comment: "Eat it, just eat it!",
+                author: "Michael Jaikishan",
+                date: "2020-09-25T17:57:28.556094Z"
+            },
+            {
+                id: 3,
+                reating: 4,
+                comment: "Ultimate, Reaching got the stars!",
+                author: "Ringo Starry",
+                date: "2020-09-25T17:57:28.556094Z"
+            },
+            {
+                id: 4,
+                reating: 1,
+                comment: "It's your birthday, we're gonna party",
+                author: "25 Cent",
+                date: "2020-09-25T17:57:28.556094Z"
+            }
+        ]
+
+    },
+    {
+        id: 2,
+        name: 'Salad',
+        image: 'assets/images/salad.png',
+        category: 'side-dish',
+        label: '',
+        price: '50.0',
+        description: 'A good combination with biriyani',
+        comments: [
+            {
+                id: 0,
+                reating: 5,
+                comment: "Imagine all the eatables, living in con",
+                author: "John Lemon",
+                date: "2020-09-25T17:57:28.556094Z"
+            },
+            {
+                id: 1,
+                reating: 4,
+                comment: "Sends anyone to heaven",
+                author: "Paul McVites",
+                date: "2020-09-25T17:57:28.556094Z"
+            },
+            {
+                id: 2,
+                reating: 3,
+                comment: "Eat it, just eat it!",
+                author: "Michael Jaikishan",
+                date: "2020-09-25T17:57:28.556094Z"
+            },
+            {
+                id: 3,
+                reating: 4,
+                comment: "Ultimate, Reaching got the stars!",
+                author: "Ringo Starry",
+                date: "2020-09-25T17:57:28.556094Z"
+            },
+            {
+                id: 4,
+                reating: 1,
+                comment: "It's your birthday, we're gonna party",
+                author: "25 Cent",
+                date: "2020-09-25T17:57:28.556094Z"
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: 'Cheese Cake',
+        image: 'assets/images/cake.png',
+        category: 'dessert',
+        label: 'cold',
+        price: '35.50',
+        description: "A delectable, semi-sweet new cheese cake",
+        comments: [
+            {
+                id: 0,
+                reating: 5,
+                comment: "Imagine all the eatables, living in con",
+                author: "John Lemon",
+                date: "2020-09-25T17:57:28.556094Z"
+            },
+            {
+                id: 1,
+                reating: 4,
+                comment: "Sends anyone to heaven",
+                author: "Paul McVites",
+                date: "2020-09-25T17:57:28.556094Z"
+            },
+            {
+                id: 2,
+                reating: 3,
+                comment: "Eat it, just eat it!",
+                author: "Michael Jaikishan",
+                date: "2020-09-25T17:57:28.556094Z"
+            },
+            {
+                id: 3,
+                reating: 4,
+                comment: "Ultimate, Reaching got the stars!",
+                author: "Ringo Starry",
+                date: "2020-09-25T17:57:28.556094Z"
+            },
+            {
+                id: 4,
+                reating: 1,
+                comment: "It's your birthday, we're gonna party",
+                author: "25 Cent",
+                date: "2020-09-25T17:57:28.556094Z"
+            }
+        ]
+    }
+]

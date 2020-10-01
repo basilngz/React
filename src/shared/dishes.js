0349456ba9fd590918316e5747932e8a1ad0bdc6
basil ng zerone <basil.g@zerone-consulting.com>
2020-10-01 19:59:07 +0530

    single page application part 1

--- src/shared/dishes.js
+++ src/shared/dishes.js
@@ -1,188 +1,43 @@
-export const DISHES = 
-[
-    {
+export const DISHES =
+    [
+        {
         id: 0,
-        name: 'Soup',
-        image: 'assets/images/soup.png',
-        category: 'starter',
-        label: 'Hot',
-        price: '60.0',
-        description: 'A unique combination of indian',
-        comments: [
-            {
-                id: 0,
-                reating: 5,
-                comment: "Imagine all the eatables, living in con",
-                author: "John Lemon",
-                date: "2020-09-25T17:57:28.556094Z"
-            },
-            {
-                id: 1,
-                reating: 4,
-                comment: "Sends anyone to heaven",
-                author: "Paul McVites",
-                date: "2020-09-25T17:57:28.556094Z"
-            },
-            {
-                id: 2,
-                reating: 3,
-                comment: "Eat it, just eat it!",
-                author: "Michael Jaikishan",
-                date: "2020-09-25T17:57:28.556094Z"
-            },
-            {
-                id: 3,
-                reating: 4,
-                comment: "Ultimate, Reaching got the stars!",
-                author: "Ringo Starry",
-                date: "2020-09-25T17:57:28.556094Z"
-            },
-            {
-                id: 4,
-                reating: 1,
-                comment: "It's your birthday, we're gonna party",
-                author: "25 Cent",
-                date: "2020-09-25T17:57:28.556094Z"
-            }
-        ]
-    },
-    {
-        id: 1,
-        name: "Biriyani",
-        image: 'assets/images/biriyani.png',
+        name:'Biriyani',
+        image: '/assets/images/biriyani.png',
         category: 'mains',
-        label: 'Hot',
-        price: '120.0',
-        description: 'A good food',
-        comments: [
-            {
-                id: 0,
-                reating: 5,
-                comment: "Imagine all the eatables, living in con",
-                author: "John Lemon",
-                date: "2020-09-25T17:57:28.556094Z"
-            },
-            {
-                id: 1,
-                reating: 4,
-                comment: "Sends anyone to heaven",
-                author: "Paul McVites",
-                date: "2020-09-25T17:57:28.556094Z"
-            },
-            {
-                id: 2,
-                reating: 3,
-                comment: "Eat it, just eat it!",
-                author: "Michael Jaikishan",
-                date: "2020-09-25T17:57:28.556094Z"
-            },
-            {
-                id: 3,
-                reating: 4,
-                comment: "Ultimate, Reaching got the stars!",
-                author: "Ringo Starry",
-                date: "2020-09-25T17:57:28.556094Z"
-            },
-            {
-                id: 4,
-                reating: 1,
-                comment: "It's your birthday, we're gonna party",
-                author: "25 Cent",
-                date: "2020-09-25T17:57:28.556094Z"
-            }
-        ]
-
-    },
-    {
+        label:'Hot',
+        price:'4.99',
+        featured: true,
+        description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                    
+        },
+        {
+        id: 1,
+        name:'Soup',
+        image: '/assets/images/soup.png',
+        category: 'appetizer',
+        label:'',
+        price:'1.99',
+        featured: false,
+        description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
+        },
+        {
         id: 2,
-        name: 'Salad',
-        image: 'assets/images/salad.png',
-        category: 'side-dish',
-        label: '',
-        price: '50.0',
-        description: 'A good combination with biriyani',
-        comments: [
-            {
-                id: 0,
-                reating: 5,
-                comment: "Imagine all the eatables, living in con",
-                author: "John Lemon",
-                date: "2020-09-25T17:57:28.556094Z"
-            },
-            {
-                id: 1,
-                reating: 4,
-                comment: "Sends anyone to heaven",
-                author: "Paul McVites",
-                date: "2020-09-25T17:57:28.556094Z"
-            },
-            {
-                id: 2,
-                reating: 3,
-                comment: "Eat it, just eat it!",
-                author: "Michael Jaikishan",
-                date: "2020-09-25T17:57:28.556094Z"
-            },
-            {
-                id: 3,
-                reating: 4,
-                comment: "Ultimate, Reaching got the stars!",
-                author: "Ringo Starry",
-                date: "2020-09-25T17:57:28.556094Z"
-            },
-            {
-                id: 4,
-                reating: 1,
-                comment: "It's your birthday, we're gonna party",
-                author: "25 Cent",
-                date: "2020-09-25T17:57:28.556094Z"
-            }
-        ]
-    },
-    {
+        name:'Vegitable Salad',
+        image: '/assets/images/salad.png',
+        category: 'appetizer',
+        label:'New',
+        price:'1.99',
+        featured: false,
+        description:'A quintessential ConFusion experience, is it a vada or is it a donut?'
+        },
+        {
         id: 3,
-        name: 'Cheese Cake',
-        image: 'assets/images/cake.png',
+        name:'Cheese Cake',
+        image: '/assets/images/cake.png',
         category: 'dessert',
-        label: 'cold',
-        price: '35.50',
-        description: "A delectable, semi-sweet new cheese cake",
-        comments: [
-            {
-                id: 0,
-                reating: 5,
-                comment: "Imagine all the eatables, living in con",
-                author: "John Lemon",
-                date: "2020-09-25T17:57:28.556094Z"
-            },
-            {
-                id: 1,
-                reating: 4,
-                comment: "Sends anyone to heaven",
-                author: "Paul McVites",
-                date: "2020-09-25T17:57:28.556094Z"
-            },
-            {
-                id: 2,
-                reating: 3,
-                comment: "Eat it, just eat it!",
-                author: "Michael Jaikishan",
-                date: "2020-09-25T17:57:28.556094Z"
-            },
-            {
-                id: 3,
-                reating: 4,
-                comment: "Ultimate, Reaching got the stars!",
-                author: "Ringo Starry",
-                date: "2020-09-25T17:57:28.556094Z"
-            },
-            {
-                id: 4,
-                reating: 1,
-                comment: "It's your birthday, we're gonna party",
-                author: "25 Cent",
-                date: "2020-09-25T17:57:28.556094Z"
-            }
-        ]
-    }
-]
+        label:'',
+        price:'2.99',
+        featured: false,
+        description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
+        }
+    ];

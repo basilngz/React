b793d25130ee36580bebfa9e1faa0ccf88da5ac9
basil ng zerone <basil.g@zerone-consulting.com>
2020-10-08 18:59:13 +0530

    Formated the codes

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,60 +1,69 @@
-import React, { Component } from 'react';
-import Home from './HomeComponent';
-import Menu from './MenuComponent';
-import Contact from './ContactComponent';
-import Dish from './DishDetailComponent';
-import Header from './HeaderComponent';
-import Footer from './FooterComponent';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { LEADERS } from '../shared/leaders';
-import { PROMOTIONS } from '../shared/promotions';
+import React, { Component } from "react";
+import Home from "./HomeComponent";
+import Menu from "./MenuComponent";
+import Contact from "./ContactComponent";
+import Dish from "./DishDetailComponent";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { LEADERS } from "../shared/leaders";
+import { PROMOTIONS } from "../shared/promotions";
 
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect } from "react-router-dom";
 
 class Main extends Component {
-  
   constructor(props) {
-    super(props)
-    
+    super(props);
+
     this.state = {
       dishes: DISHES,
       comments: COMMENTS,
       promotions: PROMOTIONS,
-      leaders: LEADERS
+      leaders: LEADERS,
     };
   }
 
-  render() { 
-
+  render() {
     const HomePage = () => {
-      return(
-        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]} 
-        promotion={this.state.promotions.filter((promo) => promo.featured)[0]} 
-        leader={this.state.leaders.filter((leader) => leader.featured)[0]} 
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
         />
       );
-    }
+    };
 
-    const DishWithId = ({match}) => {
-      return(
-        <Dish dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-        comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+    const DishWithId = ({ match }) => {
+      return (
+        <Dish
+          dish={
+            this.state.dishes.filter(
+              (dish) => dish.id === parseInt(match.params.dishId, 10)
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            (comment) => comment.dishId === parseInt(match.params.dishId, 10)
+          )}
         />
-      
-        );
-    }
+      );
+    };
 
     return (
       <div>
         <Header />
-          <Switch>
-            <Route path="/home" component={HomePage} />
-            <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
-            <Route path="/menu/:dishId" component={DishWithId} />
-            <Route exact path="/contactus" component={Contact} />
-            <Redirect to="/home" />
-          </Switch>
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route
+            exact
+            path="/menu"
+            component={() => <Menu dishes={this.state.dishes} />}
+          />
+          <Route path="/menu/:dishId" component={DishWithId} />
+          <Route exact path="/contactus" component={Contact} />
+          <Redirect to="/home" />
+        </Switch>
         <Footer />
       </div>
     );
--- src/shared/dishes.js
+++ src/shared/dishes.js
@@ -1,43 +1,46 @@
-export const DISHES =
-    [
-        {
-        id: 0,
-        name:'Biriyani',
-        image: '/assets/images/biriyani.png',
-        category: 'mains',
-        label:'Hot',
-        price:'4.99',
-        featured: true,
-        description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                    
-        },
-        {
-        id: 1,
-        name:'Soup',
-        image: '/assets/images/soup.png',
-        category: 'appetizer',
-        label:'',
-        price:'1.99',
-        featured: false,
-        description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
-        },
-        {
-        id: 2,
-        name:'Vegitable Salad',
-        image: '/assets/images/salad.png',
-        category: 'appetizer',
-        label:'New',
-        price:'1.99',
-        featured: false,
-        description:'A quintessential ConFusion experience, is it a vada or is it a donut?'
-        },
-        {
-        id: 3,
-        name:'Cheese Cake',
-        image: '/assets/images/cake.png',
-        category: 'dessert',
-        label:'',
-        price:'2.99',
-        featured: false,
-        description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
-        }
-    ];
+export const DISHES = [
+  {
+    id: 0,
+    name: "Biriyani",
+    image: "/assets/images/biriyani.png",
+    category: "mains",
+    label: "Hot",
+    price: "4.99",
+    featured: true,
+    description:
+      "A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
+  },
+  {
+    id: 1,
+    name: "Soup",
+    image: "/assets/images/soup.png",
+    category: "appetizer",
+    label: "",
+    price: "1.99",
+    featured: false,
+    description:
+      "Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce",
+  },
+  {
+    id: 2,
+    name: "Vegitable Salad",
+    image: "/assets/images/salad.png",
+    category: "appetizer",
+    label: "New",
+    price: "1.99",
+    featured: false,
+    description:
+      "A quintessential ConFusion experience, is it a vada or is it a donut?",
+  },
+  {
+    id: 3,
+    name: "Cheese Cake",
+    image: "/assets/images/cake.png",
+    category: "dessert",
+    label: "",
+    price: "2.99",
+    featured: false,
+    description:
+      "A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms",
+  },
+];
